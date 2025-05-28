d5684721f9fa0eda69c651698a193e4012461b06
DeShawn <dhouston718@yahoo.com>
2025-05-28 10:19:57 -0500

    database loaded and formatted.

--- Database/MongoDB_Connection.py
+++ Database/MongoDB_Connection.py
@@ -1,22 +1,32 @@
+import traceback
+
 from pymongo import errors
 from pymongo import MongoClient
 
-try:
-    conn_string = "mongodb://localhost:27017/"
-    client = MongoClient(conn_string)
-    print(client.list_database_names())
 
-    db = client["C964_Database"]
-    collection = db["Finance_Data"]
+def start_db():
+    try:
+        conn_string = "mongodb://localhost:27017/"
+        client = MongoClient(conn_string)
+        print(client.list_database_names())
 
-    if collection.find_one({"symbol": "TESTSYMBOL"}):
-        print("Entry exists")
+        db = client["C964_Database"]
+        collection = db["Finance_Data"]
 
-    else:
-        collection.insert_one(
-            {"symbol": "TESTSYMBOL", "current": 211.58, "previous_close": 211.45, "market_cap": 3160000000000,
-             "volume": 32276821, "RSI": 61.30, "MACD": 18.35, "EMA": 336.02, "SMA": 211.75, "Overall_Sentiment": 0.7})
+        if collection.find_one({"symbol": "TESTSYMBOL"}):
+            print("Entry exists")
+        else:
+            collection.insert_one(
+                {"symbol": "TESTSYMBOL", "current": 211.58, "previous_close": 211.45, "market_cap": 3160000000000,
+                 "volume": 32276821, "RSI": 61.30, "MACD": 18.35, "EMA": 336.02, "SMA": 211.75,
+                 "Overall_Sentiment": 0.7})
+        return db
 
-except errors.ConnectionFailure as e:
-    print(f"Error connecting to database: {e}")
+    except errors.ConnectionFailure as e:
+        print(f"Error connecting to database: {e}")
+        return None
 
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        traceback.print_exc()
+        return None
