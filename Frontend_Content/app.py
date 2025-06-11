eb1a0eb4d55ce4cd722bd10db5519e8fe2dec165
DeShawn Houston <dhouston718@yahoo.com>
2025-06-10 23:46:14 -0500

    Login page completed. Passphrase in place.

--- Frontend_Content/app.py
+++ Frontend_Content/app.py
@@ -1,16 +1,35 @@
 #Flask supports 3.9 and newer
 #Name file "app.py" to skip --app necessity
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+from Backend_Content.SecureLogin import test_passphrase
+from Database.MongoDB_Connection import start_db
 
 app = Flask(__name__)
 
-@app.route("/")
-# def landingPage():
-#     return "<p>We need to make this function robust for inserting the required text, graphs and functions on the page.</p>"
 
+@app.route("/", methods=["GET", "POST"])
 def loginPage():
-    return "Enter the student's ID to retrieve the passphrase. Then enter the passphrase below."
+    if request.method == "POST":
+        get_passphrase = request.form.get("passphrase")
+        if test_passphrase(get_passphrase):
+            return redirect(url_for("landingPage"))
+        else:
+            return "Invalid Passphrase", 403
+
+    return '''
+            <form method="POST">
+                <label for="passphrase">Enter Passphrase:</label>
+                <input type="password" name="passphrase" required>
+                <button type="submit">Submit</button>
+            </form>
+        '''
+
+
+@app.route("/landingPage", methods=["GET", "POST"])
+def landingPage():
+    return "This is the landing page"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
